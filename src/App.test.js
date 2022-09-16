e566fa3af2596065816b6dd4884cc095795af4d2
Federico Luna <fluna1973@yahoo.com>
2022-09-15 18:18:35 -0600

    set toDo in react

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import TodoContainer from './Components/App/TodoContainer';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<TodoContainer />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
