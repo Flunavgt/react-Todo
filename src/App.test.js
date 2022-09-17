a14f98cb7e17df1e49559fcf1d1a23ff9151270a
Federico Luna <fluna1973@yahoo.com>
2022-09-17 09:11:01 -0600

    Add MIT

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import TodoContainer from './Components/App/TodoContainer';
-
-test('renders learn react link', () => {
-  render(<TodoContainer />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
